1071c292adb3b519368647c77e51e58b7971be08
imxieke <oss@live.hk>
2025-04-11 11:01:39 +0800

    update brew config

--- conf/config/pkg/brew.sh
+++ conf/config/pkg/brew.sh
@@ -1,7 +1,18 @@
+#!/usr/bin/env bash
+###
+ # @Author: Cloudflying
+ # @Date: 2025-04-06 22:36:27
+ # @LastEditTime: 2025-04-11 10:14:15
+ # @LastEditors: Cloudflying
+ # @Description:
+###
 if [[ -f "/usr/local/bin/brew" ]]; then
   export HOMEBREW_NO_AUTO_UPDATE=true
   export HOMEBREW_NO_ENV_HINTS=true
+  # export HOMEBREW_INSTALL_FROM_API=1
+
   # export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+  # export HOMEBREW_API_DOMAIN="https://mirrors.aliyun.com/homebrew-bottles/api"
   export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
 
   # export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
