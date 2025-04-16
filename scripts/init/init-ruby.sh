cf5e326ed017bca30355b75f6484f84a02ee7928
imxieke <oss@live.hk>
2025-04-16 23:45:10 +0800

    add ruby init

---
+++ scripts/init/init-ruby.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+###
+ # @Author: Cloudflying
+ # @Date: 2025-04-16 22:19:32
+ # @LastEditTime: 2025-04-16 22:19:33
+ # @LastEditors: Cloudflying
+ # @Description: Ruby Init
+###
