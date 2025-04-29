35e4a2b84ec90101f2dcdcde6249f50a0f269c91
imxieke <oss@live.hk>
2025-04-29 13:48:26 +0800

    update node conf

--- conf/config/pkg/node.sh
+++ conf/config/pkg/node.sh
@@ -9,7 +9,8 @@
 
 # export NVM_NODEJS_ORG_MIRROR=http://mirrors.ustc.edu.cn/node
 # export NVM_NODEJS_ORG_MIRROR=https://registry.npmmirror.com/-/binary/node
-export NVM_NODEJS_ORG_MIRROR=https://cdn.npmmirror.com/binaries/node
+export NVM_NODEJS_ORG_MIRROR=https://mirrors.huaweicloud.com/nodejs/
+export NVM_IOJS_ORG_MIRROR=https://mirrors.huaweicloud.com/iojs/
 export NPM_CONFIG_REGISTRY=https://registry.npmmirror.com
 export NVM_DIR="${HOME}/.local/share/nvm"
 export ELECTRON_MIRROR="https://npmmirror.com/mirrors/electron/"
