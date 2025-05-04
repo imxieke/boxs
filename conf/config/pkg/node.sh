3f241b32248ec350457ca297b9000fc17f476bfe
imxieke <oss@live.hk>
2025-05-04 10:17:32 +0800

    update node env

--- conf/config/pkg/node.sh
+++ conf/config/pkg/node.sh
@@ -2,14 +2,14 @@
 ###
 # @Author: Cloudflying
 # @Date: 2024-05-31 21:18:43
-# @LastEditTime: 2025-01-01 09:45:39
-# @LastEditors: Cloudflying
+ # @LastEditTime: 2025-05-04 10:15:49
+ # @LastEditors: Cloudflying
 # @Description: Node Config
 ###
 
 # export NVM_NODEJS_ORG_MIRROR=http://mirrors.ustc.edu.cn/node
-# export NVM_NODEJS_ORG_MIRROR=https://registry.npmmirror.com/-/binary/node
-export NVM_NODEJS_ORG_MIRROR=https://mirrors.huaweicloud.com/nodejs/
+export NVM_NODEJS_ORG_MIRROR=https://registry.npmmirror.com/-/binary/node
+# export NVM_NODEJS_ORG_MIRROR=https://mirrors.huaweicloud.com/nodejs/
 export NVM_IOJS_ORG_MIRROR=https://mirrors.huaweicloud.com/iojs/
 export NPM_CONFIG_REGISTRY=https://registry.npmmirror.com
 export NVM_DIR="${HOME}/.local/share/nvm"
