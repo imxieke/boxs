dc86bcf5ff2fef462b70eb618327ce687e69d6c5
imxieke <oss@live.hk>
2025-09-29 10:30:58 +0800

    更新 locale

--- conf/config/os/env.sh
+++ conf/config/os/env.sh
@@ -8,21 +8,21 @@
 ###
 
 # Set locale and language
-# export LANG="en_US.UTF-8"
-# export LANGUAGE="en_US.UTF-8"
-# export LC_CTYPE="en_US.UTF-8"
-# export LC_NUMERIC="en_US.UTF-8"
-# export LC_TIME="en_US.UTF-8"
-# export LC_COLLATE="en_US.UTF-8"
-# export LC_MONETARY="en_US.UTF-8"
-# export LC_MESSAGES="en_US.UTF-8"
-# export LC_PAPER="en_US.UTF-8"
-# export LC_NAME="en_US.UTF-8"
-# export LC_ADDRESS="en_US.UTF-8"
-# export LC_TELEPHONE="en_US.UTF-8"
-# export LC_MEASUREMENT="en_US.UTF-8"
-# export LC_IDENTIFICATION="en_US.UTF-8"
-# export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_PAPER="en_US.UTF-8"
+export LC_NAME="en_US.UTF-8"
+export LC_ADDRESS="en_US.UTF-8"
+export LC_TELEPHONE="en_US.UTF-8"
+export LC_MEASUREMENT="en_US.UTF-8"
+export LC_IDENTIFICATION="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
 
 # export BOXS_HOME=${HOME}/.boxs
 export BOXS_HOME="$(dirname $(dirname $(dirname $(dirname $0))))"
@@ -83,7 +83,7 @@ if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
   echo "==> Fetch oh my zsh"
   git clone --depth 1 https://gitcode.com/rsync/ohmyzsh.git ~/.oh-my-zsh
   echo "==> Create .zshrc config file"
-  ln -sf ${HOME}/.boxs/conf/.zshrc ~/.zshrc
+  ln -sf "${HOME}"/.boxs/conf/.zshrc ~/.zshrc
 fi
 
 # zsh Plugin Manager
