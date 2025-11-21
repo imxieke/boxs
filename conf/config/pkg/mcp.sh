e4d7b9664b97d6430a524d46a51f6f5eb000926d
imxieke <oss@live.hk>
2025-11-21 17:45:57 +0800

    update mcp and frontend

--- conf/config/pkg/mcp.sh
+++ conf/config/pkg/mcp.sh
@@ -1,2 +1,14 @@
+#!/usr/bin/env bash
+###
+ # @Author: Cloudflying
+ # @Date: 2025-06-16 14:09:29
+ # @LastEditTime: 2025-11-21 16:36:48
+ # @LastEditors: Cloudflying
+ # @Description: Memory Settings
+###
+
 export MCP_SETTINGS_PATH=${HOME}/.config/mcp
 mkdir -p ${MCP_SETTINGS_PATH}
+
+touch ${MCP_SETTINGS_PATH}/settings.json
+touch ${MCP_SETTINGS_PATH}/memory.json
