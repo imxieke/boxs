fc5b171bc95a8930e0bff820260e54c6c053e970
imxieke <oss@live.hk>
2025-12-17 14:32:28 +0800

    add bun config

---
+++ conf/config/pkg/bun.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+###
+ # @Author: Cloudflying
+ # @Date: 2025-12-17 13:41:30
+ # @LastEditTime: 2025-12-17 13:43:24
+ # @LastEditors: Cloudflying
+ # @Description: Bun Environments
+###
+
+export BUN_INSTALL="${HOME}/.bun"
+export BUN_INSTALL_CACHE="${HOME}/.bun/install/cache"
