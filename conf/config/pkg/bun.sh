940b1efa1320f24b483dc7b6df05c90ff7aa3de9
imxieke <oss@live.hk>
2026-01-14 11:24:15 +0800

    update bun

--- conf/config/pkg/bun.sh
+++ conf/config/pkg/bun.sh
@@ -2,10 +2,12 @@
 ###
  # @Author: Cloudflying
  # @Date: 2025-12-17 13:41:30
- # @LastEditTime: 2025-12-17 13:43:24
+ # @LastEditTime: 2026-01-14 11:15:21
  # @LastEditors: Cloudflying
  # @Description: Bun Environments
 ###
 
 export BUN_INSTALL="${HOME}/.bun"
+export BUN_INSTALL_BIN="${HOME}/.bun/bin"
 export BUN_INSTALL_CACHE="${HOME}/.bun/install/cache"
+export BUN_INSTALL_GLOBAL_DIR="${HOME}/.bun/install/global"
