9e7a6b8159a549b74f4f4e5558efbbccb7b7d9ef
imxieke <oss@live.hk>
2026-01-14 11:51:43 +0800

    update node registry

--- conf/config/pkg/node.sh
+++ conf/config/pkg/node.sh
@@ -2,7 +2,7 @@
 ###
 # @Author: Cloudflying
 # @Date: 2024-05-31 21:18:43
- # @LastEditTime: 2025-05-04 10:15:49
+ # @LastEditTime: 2026-01-14 11:49:59
  # @LastEditors: Cloudflying
 # @Description: Node Config
 ###
@@ -11,8 +11,13 @@
 export NVM_NODEJS_ORG_MIRROR=https://registry.npmmirror.com/-/binary/node
 # export NVM_NODEJS_ORG_MIRROR=https://mirrors.huaweicloud.com/nodejs/
 export NVM_IOJS_ORG_MIRROR=https://mirrors.huaweicloud.com/iojs/
-# export NPM_CONFIG_REGISTRY=http://mirrors.tencent.com/npm/
-export NPM_CONFIG_REGISTRY=https://mirrors.huaweicloud.com/repository/npm/
+
+# export NPM_CONFIG_REGISTRY=https://registry.yarnpkg.com/
+# export NPM_CONFIG_REGISTRY=https://registry.npmjs.org/
+# export NPM_CONFIG_REGISTRY=https://registry.npmmirror.com/
+export NPM_CONFIG_REGISTRY=http://mirrors.tencent.com/npm/
+# export NPM_CONFIG_REGISTRY=https://mirrors.huaweicloud.com/repository/npm/
+
 export NVM_DIR="${HOME}/.local/share/nvm"
 export ELECTRON_MIRROR="https://npmmirror.com/mirrors/electron/"
 export NO_STRIP=true
