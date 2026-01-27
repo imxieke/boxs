9d48f1c81dc36c26724e2c8d1399435f53e926a7
imxieke <oss@live.hk>
2026-01-27 19:03:39 +0800

    add ollama init

---
+++ scripts/init/init-ollama.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+###
+ # @Author: Cloudflying
+ # @Date: 2026-01-27 13:50:42
+ # @LastEditTime: 2026-01-27 19:03:31
+ # @LastEditors: Cloudflying
+ # @Description: Ollama Init
+###
+
+ollama pull gpt-oss:20b-cloud
+ollama pull gpt-oss:120b-cloud
+ollama pull qwen3-coder:480b-cloud
+ollama pull qwen3-vl:235b-cloud
+ollama pull qwen3-vl:235b-instruct-cloud
+ollama pull rnj-1:8b-cloud
+ollama pull ministral-3:3b-cloud
+ollama pull ministral-3:8b-cloud
+ollama pull ministral-3:14b-cloud
+ollama pull qwen3-next:80b-cloud
+ollama pull nemotron-3-nano:30b-cloud
+ollama pull devstral-small-2:24b-cloud
+ollama pull deepseek-v3.1:671b-cloud
+ollama pull gemini-3-pro-preview:latest
+ollama pull devstral-2:123b-cloud
+ollama pull gemini-3-flash-preview:cloud
+ollama pull cogito-2.1:671b-cloud
+ollama pull glm-4.7:cloud
+ollama pull glm-4.6:cloud
+ollama pull minimax-m2:cloud
+ollama pull deepseek-v3.2:cloud
+ollama pull minimax-m2.1:cloud
+ollama pull kimi-k2-thinking:cloud
+ollama pull kimi-k2:1t-cloud
+ollama pull mistral-large-3:675b-cloud
+ollama pull gemma3:4b-cloud
+ollama pull gemma3:12b-cloud
+ollama pull gemma3:27b-cloud
