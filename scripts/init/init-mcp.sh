31b343653d10521c71ffe856756782aff04e363f
imxieke <oss@live.hk>
2026-02-01 00:52:24 +0800

    update mcp

--- scripts/init/init-mcp.sh
+++ scripts/init/init-mcp.sh
@@ -2,27 +2,53 @@
 ###
 # @Author: Cloudflying
 # @Date: 2025-11-21 14:52:05
- # @LastEditTime: 2026-01-15 10:37:57
+ # @LastEditTime: 2026-01-31 23:56:18
  # @LastEditors: Cloudflying
 # @Description: MCP Init
 ###
 
+_pip()
+{
+  pip install --break-system-packages
+}
+
+_python()
+{
+  _pip mcp-server-fetch
+  _pip mcp-server-git
+  _pip mcp-server-time
+  # https://github.com/nickclyde/duckduckgo-mcp-server
+  _pip duckduckgo-mcp-server
+  # https://github.com/modelcontextprotocol/servers-archived/tree/main/src/sqlite
+  _pip mcp-server-sqlite
+}
+
 _node()
 {
-  pnpm i -g 12306-mcp
-  pnpm i -g @mcpmarket/mcp-auto-installer
+  bun i -g 12306-mcp
+  # https://github.com/microsoft/playwright-mcp
+  bun i -g @playwright/mcp
+  bun i -g @mcpmarket/mcp-auto-installer
 
-  pnpm i -g @modelcontextprotocol/server-everything
-  pnpm i -g @modelcontextprotocol/server-memory
-  pnpm i -g @modelcontextprotocol/server-aws-kb-retrieval
-  pnpm i -g @modelcontextprotocol/server-brave-search
-  pnpm i -g @modelcontextprotocol/server-filesystem
-  pnpm i -g @modelcontextprotocol/server-gdrive
-  pnpm i -g @modelcontextprotocol/server-gitlab
-  pnpm i -g mcp-remote
-  pnpm i -g grok-search-mcp
-  pnpm i -g bing-cn-mcp
-  pnpm i -g @antv/mcp-server-antv
-  pnpm i -g @alipay/mcp-server-alipay
-  pnpm i -g mongodb-mcp-server@latest
+  bun i -g @modelcontextprotocol/server-everything
+  bun i -g @modelcontextprotocol/server-memory
+  bun i -g @modelcontextprotocol/server-aws-kb-retrieval
+  bun i -g @modelcontextprotocol/server-brave-search
+  bun i -g @modelcontextprotocol/server-filesystem
+  bun i -g @modelcontextprotocol/server-gdrive
+  bun i -g @modelcontextprotocol/server-gitlab
+  bun i -g @modelcontextprotocol/server-sequential-thinking
+  bun i -g mcp-remote
+  bun i -g grok-search-mcp
+  bun i -g bing-cn-mcp
+  bun i -g @antv/mcp-server-antv
+  bun i -g @alipay/mcp-server-alipay
+
+  # App
+  bun i -g @modelcontextprotocol/server-slack
+
+  # Database
+  bun i -g mongodb-mcp-server@latest
+  bun i -g @modelcontextprotocol/server-postgres
+  bun i -g @modelcontextprotocol/server-redis
 }
