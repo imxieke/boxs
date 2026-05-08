a926a530f6c108de46f02b8b9f750a10068b5552
imxieke <oss@live.hk>
2026-05-08 21:04:30 +0800

    update zsh sync

--- usr/local/boxs/sync/zsh.sh
+++ usr/local/boxs/sync/zsh.sh
@@ -2,13 +2,19 @@
 ###
 # @Author: Cloudflying
 # @Date: 2026-03-04 00:32:52
-# @LastEditTime: 2026-03-04 00:32:53
-# @LastEditors: Cloudflying
+ # @LastEditTime: 2026-05-08 21:02:27
+ # @LastEditors: Cloudflying
 # @Description: ZSH Sync
 ###
 
 echo "Sync Zsh History"
+
+if [[ -z "${BOXS_SECRET_HOME}" ]]; then
+  echo "Boxs Env Not Found"
+  exit 1
+fi
+
 cat ~/.zsh_history >/tmp/zsh_history
-cat ${ROOT_PATH}/home/.zsh_history >>/tmp/zsh_history
-sort -u /tmp/zsh_history >~/.zsh_history
-sort -u /tmp/zsh_history >${ROOT_PATH}/home/.zsh_history
+cat "${BOXS_SECRET_HOME}"/home/.zsh_history >>/tmp/zsh_history
+# sort -u /tmp/zsh_history > ~/.zsh_history
+sort -u /tmp/zsh_history > "${BOXS_SECRET_HOME}"/home/.zsh_history
