28af77a6fab74139982b361b19dba2b1253f89ca
imxieke <oss@live.hk>
2026-05-18 20:05:20 +0800

    update npm config

--- usr/local/boxs/config/pkg/node.sh
+++ usr/local/boxs/config/pkg/node.sh
@@ -2,7 +2,7 @@
 ###
 # @Author: Cloudflying
 # @Date: 2024-05-31 21:18:43
- # @LastEditTime: 2026-01-14 11:49:59
+ # @LastEditTime: 2026-05-18 20:05:00
  # @LastEditors: Cloudflying
 # @Description: Node Config
 ###
@@ -15,7 +15,7 @@ export NVM_IOJS_ORG_MIRROR=https://mirrors.huaweicloud.com/iojs/
 # export NPM_CONFIG_REGISTRY=https://registry.yarnpkg.com/
 # export NPM_CONFIG_REGISTRY=https://registry.npmjs.org/
 # export NPM_CONFIG_REGISTRY=https://registry.npmmirror.com/
-export NPM_CONFIG_REGISTRY=http://mirrors.tencent.com/npm/
+# export NPM_CONFIG_REGISTRY=http://mirrors.tencent.com/npm/
 # export NPM_CONFIG_REGISTRY=https://mirrors.huaweicloud.com/repository/npm/
 
 export NVM_DIR="${HOME}/.local/share/nvm"
